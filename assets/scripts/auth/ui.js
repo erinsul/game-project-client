bcd755a5fb95b696e97106739339b92fa42d573d
erinsul <erin_sullivan50@yahoo.com>
2016-05-31 15:52:13 -0400

    initial commit from api-token-auth repo

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const app = require('../app.js')
+
+const success = (data) => {
+  console.log(data);
+  $('#sign-up').children().children('.remove').val("");
+  $('#change-password').children().children('.remove').val("")
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+const signInSuccess = function(data){
+  app.user = data.user;
+  console.log(app);
+  $('#sign-in').children().children('.remove').val("")
+  $('#sign-up').hide();
+  $('#sign-in').children().children('legend').text("You are signed in as " + app.user.email);
+  $('#sign-in').children().children('input').hide();
+};
+
+const signOutSuccess = function(){
+  app.user = null;
+  console.log(app);
+  $('#sign-up').show();
+  $('#sign-in').children().children('legend').text("Sign In!");
+  $('#sign-in').children().children('input').show();
+}
+
+module.exports = {
+  failure,
+  success,
+  signInSuccess,
+  signOutSuccess,
+};
