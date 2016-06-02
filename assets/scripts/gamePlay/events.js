9d0e050da4b9c17cba107d3d0a3f29baf2555f3d
erinsul <erin_sullivan50@yahoo.com>
2016-06-02 11:49:38 -0400

    Factor out repetitive code into new file
    Factored out a bunch of repetitive code, trying to make use of functions to DRY
    it all out.

--- assets/scripts/gamePlay/events.js
+++ assets/scripts/gamePlay/events.js
@@ -1,9 +1,8 @@
 'use strict';
-const winVerify = require('./win-verification.js');
 const api = require('./api');
 const ui = require('./ui');
 const app = require('../app.js');
-let cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+const gameBoard = require('./gameBoard.js');
 
 const onNewGame = function (event){
   event.preventDefault();
@@ -13,143 +12,39 @@ const onNewGame = function (event){
 };
 
 const move = function(){
-  //data that needs to be sent with patch req
-  let gameObject = {
-    "game": {
-      "cell": {
-        "index": 0,
-        "value": ""
-    },
-      "over": false
-  }
-};
-
-const setGameObjectIndex = function(id) {
-switch(id) {
-  case "zero" :
-    gameObject.game.cell.index = 0;
-    break;
-  case "one" :
-    gameObject.game.cell.index = 1;
-    break;
-  case "two" :
-    gameObject.game.cell.index = 2;
-    break;
-  case "three" :
-    gameObject.game.cell.index = 3;
-    break;
-  case "four" :
-    gameObject.game.cell.index = 4;
-    break;
-  case "five" :
-    gameObject.game.cell.index = 5;
-    break;
-  case "six" :
-    gameObject.game.cell.index = 6;
-    break;
-  case "seven" :
-    gameObject.game.cell.index = 7;
-    break;
-  case "eight" :
-    gameObject.game.cell.index = 8;
-    break;
-}
-};
-//moving as x
-  if(app.currentMove === "x"){
     //prevents moving if already occupied
       if($(this).text() === ""){
         //sets text on board
-        $(this).text("X");
-        //sets cell value on game object
-        gameObject.game.cell.value = "x";
-        //sets index on game object
-        setGameObjectIndex(this.id);
-        //sets cells array used to test winner
-        cells[gameObject.game.cell.index] = "x";
-        //if someone won
-        if(winVerify.win(cells, "x")){
-          //sets the game over to true
-          gameObject.game.over = true;
-          console.log("x won!");
-        } else if(winVerify.tie(cells)) {
-          gameObject.game.over = true;
-          console.log("it's a tie!");
-        }
+        $(this).text(app.currentMove);
+        let id = this.id;
+        //sets game object
+        let gameObject = gameBoard.setGameObject(app.currentMove, id);
         //sends fully updated game object
         api.updateGame(gameObject)
         .done(ui.updateSuccess)
         .fail(ui.failure);
         //if game over
         if(gameObject.game.over){
-          //blocks another move
-          app.currentMove = "";
-          app.game = "";
-          $('#newGame').hide();
-          $('#clearBoard').show();
-          console.log("game over!!!");
+          gameBoard.gameOver();
         } else {
-          //otherwise, switches to o
-        app.currentMove = "o";
+        switch(app.currentMove){
+        case "x" :
+          app.currentMove = "o";
+          break;
+          //otherwise, switches to x
+        case "o" :
+        app.currentMove = "x";
+        break;
         }
     }
-  } else if (app.currentMove === "o") {
-    //prevents moving if already occupied
-    if($(this).text() === ""){
-      //sets text on board
-      $(this).text("O");
-      //sets game object cell value
-      gameObject.game.cell.value = "o";
-      //sets game object cell index
-      setGameObjectIndex(this.id);
-      //sets cells array
-      cells[gameObject.game.cell.index] = "o";
-      //if there is a winner
-      if(winVerify.win(cells, "o")){
-        //sets game object over to true
-        gameObject.game.over = true;
-      }
-      //sends game object in patch
-      api.updateGame(gameObject)
-      .done(ui.updateSuccess)
-      .fail(ui.failure);
-      //if game over
-      if(gameObject.game.over){
-        //prevent another move
-        app.currentMove = "";
-        app.game = "";
-        $('#newGame').hide();
-        $('#clearBoard').show();
-        //
-      } else {
-        //otherwise, switch to x turn
-        app.currentMove = "x";
-      }
   }
-}
 };
-
-const onClearBoard = () =>{
-  event.preventDefault();
-  cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-  $('.container').children().children('section').text("");
-  $('#clearBoard').hide();
-  $('#newGame').show();
-}
 
 const gameHandlers = () => {
 $('#newGame').on('click', onNewGame);
 // $('#joinGame').on('click', onJoinGame);
-$('#zero').on('click', move);
-$('#one').on('click', move);
-$('#two').on('click', move);
-$('#three').on('click', move);
-$('#four').on('click', move);
-$('#five').on('click', move);
-$('#six').on('click', move);
-$('#seven').on('click', move);
-$('#eight').on('click', move);
-$('#clearBoard').on('click', onClearBoard);
+$('.container').children().children('section').on('click', move);
+$('#clearBoard').on('click', gameBoard.onClearBoard);
 };
 
 module.exports = {
---
+++ assets/scripts/gamePlay/gameBoard.js
@@ -0,0 +1,85 @@
+'use strict';
+const app = require('../app.js');
+const winVerify = require('./win-verification.js');
+let cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+const setGameObjectIndex = function(object, id) {
+switch(id) {
+  case "zero" :
+    object.game.cell.index = 0;
+    break;
+  case "one" :
+    object.game.cell.index = 1;
+    break;
+  case "two" :
+    object.game.cell.index = 2;
+    break;
+  case "three" :
+    object.game.cell.index = 3;
+    break;
+  case "four" :
+    object.game.cell.index = 4;
+    break;
+  case "five" :
+    object.game.cell.index = 5;
+    break;
+  case "six" :
+    object.game.cell.index = 6;
+    break;
+  case "seven" :
+    object.game.cell.index = 7;
+    break;
+  case "eight" :
+    object.game.cell.index = 8;
+    break;
+}
+};
+
+//data that needs to be sent with patch req
+const setGameObject = (marker, id) =>{
+  let gameObject = {
+    "game": {
+      "cell": {
+    },
+      "over": false
+    }
+    };
+    //sets cell value on game object
+    gameObject.game.cell.value = marker;
+    //sets index on game object
+    setGameObjectIndex(gameObject, id);
+    //sets cells array used to test winner
+    cells[gameObject.game.cell.index] = marker;
+        //if someone won
+        if(winVerify.win(cells, marker)){
+          //sets the game over to true
+          gameObject.game.over = true;
+          console.log(marker + " won!");
+        } else if(winVerify.tie(cells)) {
+          gameObject.game.over = true;
+          console.log("it's a tie!");
+        }
+        console.log(cells);
+        return gameObject;
+};
+
+const onClearBoard = () =>{
+  event.preventDefault();
+  cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  $('.container').children().children('section').text("");
+  $('#clearBoard').hide();
+  $('#newGame').show();
+};
+
+const gameOver = () => {
+      app.currentMove = "";
+      app.game = "";
+      $('#newGame').hide();
+      $('#clearBoard').show();
+      console.log("game over!");
+};
+
+module.exports = {
+  setGameObject,
+  onClearBoard,
+  gameOver,
+};
